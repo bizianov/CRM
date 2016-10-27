b8ecd6062b4ebc22bb7f6f9b3bf838d78a852803
bizianov <viacheslav_bizianov@ukr.net>
2016-10-27 22:30:34 +0300

    refactoring after code review

--- src/main/java/project/security/PasswordGenerator.java
+++ src/main/java/project/security/PasswordGenerator.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordGenerator {
     public static void main(String[] args) {
 
-        String password = "1234";
+        String password = "1234567";
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         String encode = encoder.encode(password);
         System.out.println(encode);
