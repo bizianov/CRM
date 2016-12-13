9c4ca040a7cde9abf2ed14b39d450a8ec8305e91
bizianov <viacheslav_bizianov@ukr.net>
2016-12-13 09:34:29 +0200

    Notification Controller done

---
+++ src/main/java/project/controller/NotificationController.java
@@ -0,0 +1,90 @@
+package project.controller;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import project.model.passport.Passport;
+import project.model.tour.Tour;
+import project.model.tourist.Tourist;
+import project.service.PassportService;
+import project.service.TourService;
+import project.service.TouristService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by slava23 on 12/13/2016.
+ */
+
+@Controller
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor(staticName = "of", onConstructor = @__(@Autowired))
+public class NotificationController {
+
+    @NonNull
+    private TourService tourService;
+    @NonNull
+    private PassportService passportService;
+    @NonNull
+    private TouristService touristService;
+
+    @RequestMapping(value = "/notifications")
+    public String notificationMenu(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userLoggedIn = authentication.getName();
+        model.addAttribute("userLoggedIn", userLoggedIn);
+        model.addAttribute("oneDayToFlightTours",oneDayToFlightTours());
+        model.addAttribute("threeDaysToFlightTours",threeDaysToFlightTours());
+        model.addAttribute("returnToday", returnToday());
+        model.addAttribute("feedbackPending", feedbackPending());
+        model.addAttribute("birthdayToday", birthdayToday());
+        model.addAttribute("passportsDueToExpire",passportsDueToExpire());
+        return "notification/allNotifications";
+    }
+
+    private List<Tour> oneDayToFlightTours(){
+        return tourService.findAll()
+                .stream()
+                .filter(tour -> tour.getStartDate().equals(LocalDate.now().plusDays(1)))
+                .collect(Collectors.toList());
+    }
+
+    private List<Tour> threeDaysToFlightTours(){
+        return tourService.findAll()
+                .stream()
+                .filter(tour -> tour.getStartDate().equals(LocalDate.now().plusDays(3)))
+                .collect(Collectors.toList());
+    }
+
+    private List<Tour> returnToday(){
+        return tourService.findAll()
+                .stream()
+                .filter(tour -> tour.getEndDate().equals(LocalDate.now()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Tour> feedbackPending(){
+        return tourService.findAll()
+                .stream()
+                .filter(tour -> tour.getEndDate().equals(LocalDate.now().minusDays(2)))
+                .collect(Collectors.toList());
+    }
+
+    private List<Tourist> birthdayToday(){
+        return touristService.findTouristsByBirthday();
+    }
+
+    private List<Passport> passportsDueToExpire(){
+        return passportService.getPassportsDueToExpire();
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/notification/allNotifications.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Notifications</title>
+</head>
+<body>
+<p align="center"><a href="/main.jsp">Main menu</a>
+<hr>
+<b>1 day to flight</b>
+<hr>
+<b>3 days to flight</b>
+<hr>
+<b>Return today</b>
+<hr>
+<b>Birthday today</b>
+<hr>
+<b>Ask for feedback</b>
+<hr>
+<b>Passports due to expire</b>
+<hr>
+</body>
+</html>
